ba1855cc6a4010db6d8fa57b3ff0317200ec286a
ggmu2016 <ggmu2016@utexas.edu>
2024-04-23 17:20:26 -0500

    minor changes

--- rnn.py
+++ rnn.py
@@ -47,17 +47,17 @@ class RNN:
 
         return np.array(outputs), np.array(hidden_states)
 
-    def back_prop(self, W_xh, W_hh, W_hy, dL, h_t):
+    def back_prop(self, dL, h_t):
         for t in range(self.seq_length-1,-1,-1):
             if t == (self.seq_length-1):
                 dh_next = 0  # temp
-            dW_hy = np.matmul(np.array(dL), np.array(h_t[t]).transpose())
-            dh_t = np.matmul(np.array(W_hy).transpose(), np.array(dL)) + np.array(dh_next)
+            dW_hy = np.matmul(dL, h_t[t].T)
+            dh_t = np.matmul(self.W_hy.T, dL) + dh_next
             dh_raw = np.matmul((1-np.matmul(np.arrar(h_t[t]), np.array(h_t[t]))), dh_t)
             db_hh = dh_raw
             dW_xh = np.matmul(dh_raw, np.array(self.X[t]).transpose())
             dW_hh = np.matmul(dh_raw, np.array(h_t[t-1]).transpose())
-            dh_next = np.matmul(np.array(W_hh).transpose(), dh_raw)
+            dh_next = np.matmul(np.array(self.W_hh).transpose(), dh_raw)
 
         return dW_xh, dW_hh, dW_hy, db_hh
     def calc_loss(self, y_target, y_model, loss_function = "MSE"):
