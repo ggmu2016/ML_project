6f67e12bb8c496e93d85680265f57440668ec0cb
daryaanbar <anbardarya22@gmail.com>
2024-04-25 22:18:17 -0500

    Added methods to generate sequences and branches

--- preprocess.py
+++ preprocess.py
@@ -1,24 +1,19 @@
-'''
-This file will contain the pre_process class and its methods
-- normalize
-- eliminate_outliers
-- eliminate some columns/select the proper columns
-- split into test and train
-- etc
-'''
-
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import MinMaxScaler, LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.impute import SimpleImputer
-import sys
 
 
 class PreProcess:
-    def __init__(self, file_path, train_split_size):
+    
+    def __init__(self, file_path, train_split_size, batch_size, seq_length):
         self.file_path = file_path
+        # hyperparameters:
         self.train_split_size = train_split_size
+        self.seq_length = seq_length
+        self.batch_size = batch_size
+        # data:
         self.data = None    # original dataset
         self.X = None
         self.y = None
@@ -26,12 +21,14 @@ class PreProcess:
         self.X_test = None
         self.y_train = None
         self.y_test = None
-
+        self.X_train_seq = None
+        self.y_train_seq = None
+        self.X_train_batches = None
+        self.y_train_batches = None
 
     def load_data(self):
         data = pd.read_csv(self.file_path)
         self.data = pd.DataFrame(data)
-
 
     def handle_missing_values(self):
         # remove rows with missing labels and reset indices
@@ -50,46 +47,45 @@ class PreProcess:
         # fill in missing dates appropriately
         self.X[0] = self.X[0].replace("", None).ffill().bfill() # fills forward then backward
 
-
-    def normalize(self, X_train, X_test, y_train, y_test):
-        # rescales training and test data to [0, 1]
-        scaler = MinMaxScaler()
-
-        self.X_train = scaler.fit_transform(X_train)
-        self.X_test = scaler.fit_transform(X_test)
-        self.y_train = scaler.fit_transform(y_train)
-        self.y_test = scaler.fit_transform(y_test)
-      
-
-    def eliminate_outliers(self):
-        pass
-
-
     def transform_dates(self):
         # encodes dates sequentially, starting from 0...
         label_encoder = LabelEncoder()
         self.X[0] = pd.to_datetime(self.X[0], format='%d-%m-%Y')
         self.X[0] = label_encoder.fit_transform(self.X[0])
 
-
     def split_data(self):
-        X_train, X_test, y_train, y_test = train_test_split(self.X, self.y, train_size = self.train_split_size)
-        return X_train, X_test, y_train, y_test
+        # splits data into training and testing sets, maintaining sequential order
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.X, self.y, train_size = self.train_split_size, shuffle=False)
+    
+    def normalize(self):
+        # rescales training and test data to [0, 1]
+        scaler = MinMaxScaler()
+        self.X_train = scaler.fit_transform(self.X_train)
+        self.X_test = scaler.fit_transform(self.X_test)
+        self.y_train = scaler.fit_transform(self.y_train)
+        self.y_test = scaler.fit_transform(self.y_test)
 
+    def create_sequences(self):
+        # iterates over training data using sliding window approach
+        X_seq = []
+        y_seq = []
+        for i in range(len(self.X_train) - self.seq_length):
+            X_seq.append(self.X_train[i:i+self.seq_length])
+            y_seq.append(self.y_train[i + self.seq_length])
+        self.X_train_seq = np.array(X_seq)
+        self.y_train_seq = np.array(y_seq)
+    
+    def create_batches(self):
+        # creates batches of the training sequences
+        num_batches = len(self.X_train_seq) // self.batch_size
+        self.X_train_batches = np.array_split(self.X_train_seq[:num_batches * self.batch_size], num_batches)
+        self.y_train_batches = np.array_split(self.y_train_seq[:num_batches * self.batch_size], num_batches)
 
-def main():
-    prepro = PreProcess(sys.argv[1], 0.8)
-    prepro.load_data()
-    prepro.handle_missing_values()
-    prepro.transform_dates()
-    X_train, X_test, y_train, y_test = prepro.split_data()
-    prepro.normalize(X_train, X_test, y_train, y_test)
-
-    print(prepro.X_train, "\n")
-    print(prepro.X_test, "\n")
-    print(prepro.y_train, "\n")
-    print(prepro.y_test)
-
-
-if __name__ == "__main__":
-    main()
+    def preprocess_data(self):
+        self.load_data()
+        self.handle_missing_values()
+        self.transform_dates()
+        self.split_data()
+        self.normalize()
+        self.create_sequences()
+        self.create_batches()
